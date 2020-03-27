b47df91b75a6de6c47b7995445652f1365cec7db
tcsanthony <anthonyshing2006@gmail.com>
2020-03-28 02:02:23 +0800

    last

--- api.py
+++ api.py
@@ -20,4 +20,4 @@ api.add_resource(Igsearch, '/search/<username>')
 api.add_resource(Questionnaire, '/questionnaire/answer')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
--- application.py
+++ application.py
@@ -130,4 +130,4 @@ def contact():
     return render_template('contact.html')
 
 if __name__ == '__main__':
-   app.run(debug=True, port=80)
+   app.run(host="0.0.0.0", port=80)
