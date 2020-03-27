85eee037107381551b7ddac7e4f275d83a46c2cd
tcsanthony <anthonyshing2006@gmail.com>
2020-03-27 18:53:45 +0800

    push

--- api.py
+++ api.py
@@ -20,4 +20,4 @@ api.add_resource(Igsearch, '/search/<username>')
 api.add_resource(Questionnaire, '/questionnaire/answer')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=80, debug=True)
